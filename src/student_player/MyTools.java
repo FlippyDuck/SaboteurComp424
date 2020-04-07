4d460cb085451d15e24921a376c32178ae1e4ebb
Murko\Murray K <murray.kornelsen@mail.mcgill.ca>
2020-04-07 18:39:41 -0400

    murray changes

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,79 @@
 package student_player;
 
+import java.util.ArrayList;
+
+import Saboteur.SaboteurBoardState;
+import Saboteur.SaboteurMove;
+import Saboteur.cardClasses.SaboteurBonus;
+import Saboteur.cardClasses.SaboteurCard;
+import Saboteur.cardClasses.SaboteurDrop;
+import Saboteur.cardClasses.SaboteurMap;
+import Saboteur.cardClasses.SaboteurTile;
+
 public class MyTools {
     public static double getSomething() {
         return Math.random();
+    }
+
+    public static double computeEvaluation(SaboteurMove move, SaboteurBoardState boardState) {
+        
+        SaboteurCard card = move.getCardPlayed();
+        int[] pos = move.getPosPlayed();
+        
+        return 0;
+    }
+
+    public static SaboteurMove moveTowardsGoal(SaboteurBoardState boardState) {
+        // ArrayList<SaboteurMove> moves = boardState.getAllLegalMoves();
+        // SaboteurMove bestMove = null;
+        // double bestEval = 0.0;
+        // for (int i = 0; i < moves.size(); i++) {
+        //     double tmp = MyTools.computeEvaluation(moves.get(i), boardState);
+        //     if (tmp > bestEval) {
+        //         bestMove = moves.get(i);
+        //         bestEval = tmp;
+        //     }
+        // }
+
+        SaboteurTile[][] board = boardState.getBoardForDisplay();
+        
+        ArrayList<SaboteurCard> cards = boardState.getCurrentPlayerCards();
+
+        ArrayList<SaboteurTile> pathTiles = new ArrayList<>();
+
+        if (boardState.getNbMalus(boardState.getTurnPlayer()) > 0) {
+            for (int i = 0; i < cards.size(); i++) {
+                if (cards.get(i).getName().equals("Bonus")) {
+                    return new SaboteurMove(new SaboteurBonus(), 0, 0, boardState.getTurnPlayer());
+                }
+            }
+            
+        }
+        for (int i = 0; i < cards.size(); i++) {
+            if (cards.get(i).getName().equals("Map")) {
+                SaboteurMove move;
+                if (!board[SaboteurBoardState.hiddenPos[1][0]][SaboteurBoardState.hiddenPos[1][1]].getIdx().equals("goalTile")) {
+                    move = new SaboteurMove(new SaboteurMap(), SaboteurBoardState.hiddenPos[1][0], SaboteurBoardState.hiddenPos[1][1], boardState.getTurnPlayer());     
+                } else if (!board[SaboteurBoardState.hiddenPos[0][0]][SaboteurBoardState.hiddenPos[0][1]].getIdx().equals("goalTile")) {
+                    move = new SaboteurMove(new SaboteurMap(), SaboteurBoardState.hiddenPos[0][0], SaboteurBoardState.hiddenPos[0][1], boardState.getTurnPlayer());
+                } else if (!board[SaboteurBoardState.hiddenPos[2][0]][SaboteurBoardState.hiddenPos[2][1]].getIdx().equals("goalTile")) {
+                    move = new SaboteurMove(new SaboteurMap(), SaboteurBoardState.hiddenPos[2][0], SaboteurBoardState.hiddenPos[2][1], boardState.getTurnPlayer());
+                } else {
+                    move = new SaboteurMove(new SaboteurDrop(), i, 0, boardState.getTurnPlayer());
+                }
+                return move;
+                // for (int j = 0; j < 3; j++) {
+                //     System.out.println(board[boardState.hiddenPos[j][0]][boardState.hiddenPos[j][1]].getIdx());
+                                
+                // }
+            } else if (cards.get(i).getName().startsWith("Tile")) {
+                pathTiles.add((SaboteurTile) cards.get(i));
+            }
+        }
+        
+        for (int i = 0; i < pathTiles.size(); i++) {
+            ArrayList<int[]> positions = boardState.possiblePositions(pathTiles.get(i));
+            
+        }
     }
 }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -3,11 +3,18 @@ package student_player;
 import boardgame.Move;
 
 import Saboteur.SaboteurPlayer;
+
+import java.util.ArrayList;
+
 import Saboteur.SaboteurBoardState;
+import Saboteur.SaboteurMove;
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends SaboteurPlayer {
 
+    private enum Phase {OPENING, MIDGAME, ENDGAME}
+
+    private Phase currentPhase;
     /**
      * You must modify this constructor to return your student number. This is
      * important, because this is what the code that runs the competition uses to
@@ -15,6 +22,7 @@ public class StudentPlayer extends SaboteurPlayer {
      */
     public StudentPlayer() {
         super("260714814");
+        currentPhase = Phase.OPENING;
     }
 
     /**
@@ -22,16 +30,30 @@ public class StudentPlayer extends SaboteurPlayer {
      * object contains the current state of the game, which your agent must use to
      * make decisions.
      */
+    @Override
     public Move chooseMove(SaboteurBoardState boardState) {
         // You probably will make separate functions in MyTools.
         // For example, maybe you'll need to load some pre-processed best opening
         // strategies...
-        MyTools.getSomething();
+
+        // MyTools.getSomething();
 
         // Is random the best you can do?
-        Move myMove = boardState.getRandomMove();
+        // Move myMove = boardState.getRandomMove();
 
+        SaboteurMove result = null;
+
+        switch(currentPhase) {
+            case OPENING:
+                
+                break;
+            case MIDGAME:
+                break;
+            case ENDGAME:
+                break;
+        }
+    
         // Return your move to be processed by the server.
-        return myMove;
+        return result;
     }
 }
