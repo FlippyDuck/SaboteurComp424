670b81d84544bc4410ea1ed245c4154b8fabbbff
TianyiZhang2 <tianyi.zhang2@mail.mcgill.ca>
2020-04-09 15:02:31 -0400

    Move monteCarlo method to MyTools

--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -18,8 +18,6 @@ public class StudentPlayer extends SaboteurPlayer {
     private enum Phase {OPENING, MIDGAME, ENDGAME}
 
 	private Phase currentPhase;
-	private ArrayList<SaboteurCard> discard = new ArrayList<SaboteurCard>();
-    private SaboteurTile[][] previousBoard;
     /**
      * You must modify this constructor to return your student number. This is
      * important, because this is what the code that runs the competition uses to
@@ -67,77 +65,7 @@ public class StudentPlayer extends SaboteurPlayer {
                 
                 break;
             case MIDGAME:
-                //Getting opponent's last move
-		if (previousBoard != null) {
-			SaboteurTile[][] board = boardState.getHiddenBoard();
-			for(int i = 0; i < SaboteurBoardState.BOARD_SIZE; i++) {
-				for(int j = 0; j < SaboteurBoardState.BOARD_SIZE; j++) {
-					if(previousBoard[i][j] != board[i][j]) {
-						//Tile was added
-						if(previousBoard[i][j] == null) {
-							discard.add(board[i][j]);
-						}
-						//Tile was destroyed
-						else if(board[i][j] == null) {
-							discard.add(new SaboteurDestroy());
-						}
-						//TODO: enemy map, bonus, malus
-						//Don't know what cards enemy has dropped
-					}
-				}
-			}
-		}
-		//Getting rest of deck
-		ArrayList<SaboteurCard> myCards = boardState.getCurrentPlayerCards();
-		ArrayList<SaboteurCard> deck = SaboteurCard.getDeck();
-		//Remove cards from discard pile
-        for(int i = 0; i < deck.size(); i++) {
-            for(int j = 0; j < discard.size(); j++) {
-                if(deck.get(i).getName().equals(discard.get(j).getName())) {
-                    deck.remove(i);
-                    i--;
-                    break;
-                }
-            }
-		}
-		//Remove cards from your hand
-        for(int i = 0; i < deck.size(); i++) {
-            for(int j = 0; j < myCards.size(); j++) {
-                if(deck.get(i).getName().equals(myCards.get(j).getName())) {
-                    deck.remove(i);
-                    i--;
-                    break;
-                }
-            }
-		}
-		//Monte carlo
-    	SaboteurMove myMove = boardState.getRandomMove();
-    	double max = 0;
-    	for(SaboteurMove move : boardState.getAllLegalMoves()) {
-    		//Prioritize map
-    		if(move.getCardPlayed() instanceof SaboteurMap) {
-    			myMove = move;
-    			break;
-    		}
-    		BoardCopy board;
-    		double utility = 0;
-    		//Number of random runs per legal move
-    		int numRuns = 100;
-    		for(int i = 0; i < numRuns; i++) {
-    			board = new BoardCopy(boardState.getHiddenBoard(), boardState.getHiddenIntBoard(), boardState.getCurrentPlayerCards(), deck, player_id);
-    			//Process your move, then start the random run
-    			board.processMove(move);
-    			utility += board.run();
-    		}
-    		if(utility > max) {
-    			myMove = move;
-    			max = utility;
-    		}
-    		//System.out.println(max);
-    	}
-		discard.add(myMove.getCardPlayed());
-		previousBoard = boardState.getHiddenBoard();
-                result = myMove;
+                result = MyTools.monteCarlo(boardState, player_id);
             case ENDGAME:
                 break;
         }
